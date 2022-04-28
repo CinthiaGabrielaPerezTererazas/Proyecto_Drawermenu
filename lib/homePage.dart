9f92537919aeeb1d28e9a1109a63ff5a0b0ad7b2
FlutLab.io <>
2022-04-28 14:27:58 +0000

    Initial commit

---
+++ lib/homePage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:perez/navigationDrawer.dart';
+
+class homePage extends StatelessWidget {
+  static const String routeName = '/homePage';
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(
+          actions: [
+            Icon(Icons.search),
+            Icon(Icons.more_vert)
+          ],
+          elevation: 8,
+          shadowColor: Colors.black,
+          backgroundColor: Colors.red,
+          title: Text("INICIO CentralC"),
+        ),
+        drawer: navigationDrawer(),
+        body: Center(child: Text("Falta poner logos e imagenes")));
+  }
+}
---
+++ lib/pageRoute.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:perez/homePage.dart';
+import 'package:perez/contactPage.dart';
+import 'package:perez/eventPage.dart';
+import 'package:perez/profilePage.dart';
+import 'package:perez/notificationPage.dart';
+
+class pageRoutes {
+  static const String home = homePage.routeName;
+  static const String contact = contactPage.routeName;
+  static const String event = eventPage.routeName;
+  static const String profile = profilePage.routeName;
+  static const String notification = notificationPage.routeName;
+}
